ba6e751c8586ddf9ea5ea0594ca898535530ccba
Nedeljko Šovljanski <sovljaned@gmail.com>
2022-01-19 17:50:42 +0100

    Add project files.

---
+++ InventorAddIninVB/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("InventorAddIninVB")>
+<Assembly: AssemblyDescription("InventorAddIninVB")>
+<Assembly: AssemblyCompany("Autodesk, Inc.")>
+<Assembly: AssemblyProduct("Autodesk Inventor")>
+<Assembly: AssemblyCopyright("Copyright ?2012 Autodesk, Inc.")>
+<Assembly: AssemblyTrademark("")>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ba41b780-db23-43f6-9501-9a310eba31ad")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0.*")>
